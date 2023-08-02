4b881f30917ae4685868090dc2c3c541c2ac8910
Saheb Giri <iamsahebgiri@gmail.com>
2023-08-02 19:01:36 +0530

    chore: disable image optimization globally

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true, // I ran out of my image optimization quotas in vercel
     remotePatterns: [
       {
         hostname: "*.unsplash.com",
